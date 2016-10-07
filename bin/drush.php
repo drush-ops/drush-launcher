6a7badb6493ef187e93f92260cde892f0c50b315
Florian Weber <florian.weber@ueberbit.de>
2016-10-07 23:15:51 +0200

    Fix autoloader discovery and error messages

--- bin/drush.php
+++ bin/drush.php
@@ -4,21 +4,52 @@ use DrupalFinder\DrupalFinder;
 
 set_time_limit(0);
 
-require_once __DIR__ . '/../vendor/autoload.php';
+$autoloaders = [
+  __DIR__ . '/../../../autoload.php',
+  __DIR__ . '/../vendor/autoload.php'
+];
+
+foreach ($autoloaders as $file) {
+  if (file_exists($file)) {
+    $autoloader = $file;
+    break;
+  }
+}
+
+if (isset($autoloader)) {
+  require_once $autoloader;
+}
+else {
+  echo 'You must set up the project dependencies using `composer install`' . PHP_EOL;
+  exit(1);
+}
 
 $drupalFinder = new DrupalFinder();
 $drupalRoot = $drupalFinder->locateRoot(getcwd());
 
 if (!$drupalRoot) {
-  echo PHP_EOL . 'Could not find Drupal root' . PHP_EOL;
+  echo 'Could not find Drupal in the current path.' . PHP_EOL;
   exit(1);
 }
 
 chdir($drupalRoot);
 
 require_once $drupalRoot . '/autoload.php';
-require_once $drupalRoot . '/../vendor/drush/drush/includes/preflight.inc';
-require_once $drupalRoot . '/../vendor/drush/drush/includes/context.inc';
+
+$files = [
+  $drupalRoot . '/../vendor/drush/drush/includes/preflight.inc',
+  $drupalRoot . '/../vendor/drush/drush/includes/context.inc'
+];
+
+foreach ($files as $file) {
+  if (file_exists($file)) {
+    require_once $file;
+  }
+  else {
+    echo 'The Drupal site has not local drush.' . PHP_EOL;
+    exit(1);
+  }
+}
 
 drush_set_option('root', $drupalRoot);
 drush_set_option('local', TRUE);
